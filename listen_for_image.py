fed55b6b66c49db192d5ce57e0063604f82b1023
Tao Zhang <taozhang1917@gmail.com>
2019-09-15 09:19:59 -0400

    some edits

--- listen_for_image.py
+++ listen_for_image.py
@@ -8,9 +8,8 @@ from watchdog.events import FileSystemEventHandler
 
 block_blob_service = BlockBlobService(account_name='htn2019', account_key='1jvcfmZQrfPUSZdv8Ofdv3L7047OkmMfY66ZEeYYSf3DFMF+kia/5qzO68UsX8fL4utjCq1iTOUQdHIVdbciTQ==')
 blob_name = "captures"
-j = 0
 n = 7
-
+j = 0
 class Watcher:
     dir = "./images"
 
@@ -32,7 +31,6 @@ class Watcher:
 
 
 class Handler(FileSystemEventHandler):
-
     @staticmethod
     def on_any_event(event):
         if event.is_directory:
@@ -47,7 +45,7 @@ class Handler(FileSystemEventHandler):
 
             # Take any action here when a file is first created.
             print("Received created event - $s." + str(event.src_path))
-            block_blob_service.create_blob_from_path("captures", "t" + str(i), event.src_path)
+            block_blob_service.create_blob_from_path("gallery", "g" + str(j) + ".jpg", event.src_path)
             # upload_file(event.src_path, block_blob_service, blob_name, image_name)
             # block_blob_service.get_blob_to_path(blob_name, os.path.basename(event.src_path),
             #                                     os.path.join(os.path.expanduser("~/Pictures/target"), os.path.basename(event.src_path)))
@@ -55,10 +53,6 @@ class Handler(FileSystemEventHandler):
         # elif event.event_type == 'modified':
             # Taken any action here when a file is modified.
 
-def upload_7():
-    for i in range(n):
-        upload_file("/home/sniprs/anaconda3/envs/storage-blobs-python-quickstart/gallery/g" + str(i) + ".jpg", block_blob_service, "gallery")
-        print(str(block_blob_service.make_blob_url("gallery", "g" + str(i))))
 
 def watch_for_stuff():
     w = Watcher()
